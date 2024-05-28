8b5fee67fcc5f4ba369f87a9e31363feb968ed9d
Valerchik :) <57862483+valeri3@users.noreply.github.com>
2024-05-28 02:35:07 +0200

    Add files via upload

---
+++ HW3/home-work3/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Section} from "./components/products";
+
+function App() {
+    return (
+        <div className="App">
+            <Section/>
+        </div>
+    );
+}
+
+export default App;
